92161bd092283c5b948dbc1a9a34e380e454af4c
Guilherme Sá <gui.santos.sa@gmail.com>
2021-02-12 13:09:21 -0300

    login

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
